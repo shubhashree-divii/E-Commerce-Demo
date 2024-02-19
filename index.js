b5c9f3840128fbd5cb500fd645730248442b995b
Shubhashree <shubhashree@divii.com>
2024-02-20 02:11:04 +0530

    added user routes to main app

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 require("./db/mongoose");
 
+const userRouter = require('./routes/user')
+
 const app = express();
 app.use(express.json());
+app.use(userRouter)
 
 const port = process.env.PORT;
 
