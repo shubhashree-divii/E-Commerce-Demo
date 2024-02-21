c6dd3eaa6befe3dd253c1fd67ac2a5387c54a7ad
Shubhashree <shubhashree@divii.com>
2024-02-22 02:25:08 +0530

    added login route to user routes

--- routes/user.js
+++ routes/user.js
@@ -4,15 +4,25 @@ const User = require("../models/user");
 const router = new express.Router();
 
 //signup
-router.post("/users", async (req, res) => {
+router.post("/register", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send({error:error.message});
   }
 });
 
-module.exports = router
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.login(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({message:"Login Successful", user, token });
+  } catch (error) {
+    res.status(400).send({error:error.message});
+  }
+});
+
+module.exports = router;
