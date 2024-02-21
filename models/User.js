678fd298af339228215893b7309feaefcf864c88
Shubhashree <shubhashree@divii.com>
2024-02-22 02:15:10 +0530

    added encryption for password field

--- models/User.js
+++ models/User.js
@@ -46,6 +46,14 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
