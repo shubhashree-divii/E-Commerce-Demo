4d80ccad89553f4b10f1beb30f445252f0b4b727
Shubhashree <shubhashree@divii.com>
2024-02-22 02:35:01 +0530

    added logout route

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/user");
+const isAuthenticated = require("../middleware/isAuthenticated");
 
 const router = new express.Router();
 
@@ -24,5 +25,17 @@ router.post("/login", async (req, res) => {
     res.status(400).send({error:error.message});
   }
 });
+
+router.post('/logout', isAuthenticated, async (req, res) => {
+  try {
+      req.user.tokens =  req.user.tokens.filter((token) => {
+     return token.token !== req.token
+    })
+      await req.user.save()
+      res.send({status:true,message:"Succesfully logged out!!"})
+  } catch (error) {
+      res.status(500).send({error:error.message})
+  }
+  })
 
 module.exports = router;
